dac2977c83666d5b48a8a214c38dbb72ff8b397e
lotarv <lotarev200445.gmail.com>
2025-04-30 17:18:19 +0300

    fix:design

--- app/src/views/MembersView.vue
+++ app/src/views/MembersView.vue
@@ -62,7 +62,7 @@ onBeforeMount(async () => {
 </script>
 
 <template>
-    <section class="p-2 flex flex-col gap-4 font-medium">
+    <section class="p-1 flex flex-col gap-1 font-medium">
         <h1 class="header">Участники</h1>
         <div v-if="isLoading" class="text-center text-xl">Загрузка...</div>
         <div v-else-if="error" class="error">{{ error }}</div>
@@ -72,11 +72,11 @@ onBeforeMount(async () => {
                 :to="`/declarations/${member.username}`"
                 class="member-card" v-for="member in members.reverse()" :key="member.username">
                     <div class="member-header">
-                        <div class="flex items-center gap-2">
+                        <div class="flex items-center gap-3">
                             <img class="member-photo" :src="member.avatar_url" alt="">
                             <div class="member-declaration">
-                                <span class="text-gray-500 tracking-[-0.4px]">Декларация</span>
-                                <span>до 01.01.2001</span>
+                                <span class="text-[hsl(0,0%,50%)] tracking-[-0.4px]">Декларация</span>
+                                <span>до 26.12.2025</span>
                             </div>
                         </div>
                         <div class="self-start text-2xl"><ArrowIcon/></div>
@@ -101,11 +101,11 @@ onBeforeMount(async () => {
 }
 
 .members-container{
-    @apply flex flex-col gap-2 justify-center align-top
+    @apply flex flex-col gap-1 justify-center align-top
 }
 
 .member-card{
-    @apply flex flex-col bg-white rounded-xl p-5 gap-4 cursor-pointer
+    @apply flex flex-col bg-white rounded-[16px] p-[16px] gap-5 cursor-pointer
 }
 
 .member-card{
@@ -121,7 +121,7 @@ onBeforeMount(async () => {
 }
 
 .member-declaration{
-    @apply flex flex-col text-base
+    @apply flex flex-col text-[16px] leading-6
 }
 
 .members-about{
@@ -129,13 +129,13 @@ onBeforeMount(async () => {
 }
 
 .member-name {
-    @apply text-xl tracking-[-0.4px]
+    @apply text-xl tracking-[-0.4px] leading-6
 }
 .member-name {
     font-family: 'SF Pro Display', Roboto, emoji, sans-serif;
 }
 .member-income{
-    @apply flex flex-row gap-1 text-[14px] font-[600] tracking-[-0.25px]
+    @apply flex flex-row gap-1 text-[14px] font-[500] tracking-[-0.25px] leading-5
 }
 .dot {
     @apply w-[4px] h-[4px] bg-black inline-block rounded-[50%] self-center
