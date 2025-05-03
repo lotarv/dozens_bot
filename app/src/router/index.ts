7eba0a24a122a4b9246bf1861908bdc2c09280b8
lotarv <lotarev200445.gmail.com>
2025-05-03 13:16:43 +0300

    router fix

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -2,7 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 const routes = [
     {
         path: '/',
-        name:'main',
+        name:'MainView',
         component: () => import('../views/MainView.vue')
     },
     {
