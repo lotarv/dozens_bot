a5ecc2197c4bdc6250fac568ac8edcc3082d40af
lotarv <lotarev200445@gmail.com>
2025-07-08 16:04:51 +0300

    fix:migration

--- api/migrations/20250707181050_create_user_states_table.sql
+++ api/migrations/20250707181050_create_user_states_table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE user_state (
     telegram_id BIGINT PRIMARY KEY,
     current_state TEXT NOT NULL,
-    updated_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
 -- +goose StatementEnd
 
