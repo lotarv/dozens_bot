13bdad01c28a6c875fbc3dca4f7208f1b2f3a697
lotarv <lotarev200445@gmail.com>
2025-07-21 16:02:56 +0300

    fix(design): login page

--- app/src/features/loginPage/views/LoginView.vue
+++ app/src/features/loginPage/views/LoginView.vue
@@ -12,21 +12,45 @@ function openChatBot() {
     }
 }
 
+function openLink() {
+    const url = `https://t.me/incetro`;
+    if (window.Telegram && window.Telegram.WebApp) {
+        Telegram.WebApp.openTelegramLink(url);
+        Telegram.WebApp.close();
+    } else {
+        window.location.href = url;
+    }
+}
 </script>
 <template>
     <main class="login-main">
         <div class="info-block">
-            <p class="fond-medium text-[28px] leading-[32px] tracking-[-0.4px]">Мы знакомы?</p>
-            <p class="font-medium text-gray-400 text-base tracking-[-0.4px]">Похоже, вы не являетесь членом десятки. Вы можете присоединиться к своей десятке по коду или создать новую в нашем боте!</p>
-            <button class="go-button" @click="openChatBot">Погнали!</button>
+            <img src="../assets/logo.png" alt="" class="w-12 h-12">
+            <p class="fond-medium text-[28px] leading-[32px] tracking-[-0.4px]">Упс</p>
+            <p class="font-medium text-gray-400 text-base tracking-[-0.4px] text-center px-2">Пока что бот доступен только для некоторых Десяток. Если хотите попробовать, пишите <span class="font-bold" @click="openLink">@incetro</span></p>
         </div>
     </main>
 </template>
 <style scoped>
 .login-main {
     @apply bg-center bg-cover flex flex-col items-center justify-center px-4 overflow-hidden rounded-xl;
-    background-image: url(../assets/login-bg.png);
+    position:relative;
+    z-index: 1;
 }
+.login-main::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-image: url(../assets/login-bg.png);
+    background-size: cover;
+    background-position: center;
+    filter: blur(30px); /* Уровень размытия, можно настроить */
+    z-index: -1; /* Помещаем фон под содержимое */
+}
+
 
 .info-block {
     @apply bg-white rounded-2xl flex flex-col gap-2 items-center text-center p-6
